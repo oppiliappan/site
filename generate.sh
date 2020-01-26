e9bd47266207dbdcae055c865ca3af80918340b5
Akshay <nerdypepper@tuta.io>
2020-01-26 12:00:02 +0530

    add read time, post length, remove commit sha

--- generate.sh
+++ generate.sh
@@ -7,11 +7,21 @@ title_wrapper() {
     echo "$1" | sed -E -e "s/\..+$//g"  -e "s/_(.)/ \u\1/g" -e "s/^(.)/\u\1/g"
 }
 
+read_time() {
+    minu="$(eva -f 1 $1/150 | xargs)"
+    echo "$minu"
+}
+
+height() {
+    cm="$(eva -f 2 $1*18*0.0222 | xargs)"
+    echo "$cm"
+}
+
 link_wrapper() {
     # 1 - id
     # 2 - title
     # 3 - date
-    # 4 - commit hash
+    # 4 - read time
     echo -ne "
     <div class=\"post\">
         <div class=\"date\">
@@ -20,11 +30,12 @@ link_wrapper() {
         <a href=\"/posts/$1\" class=\"post-link\">
             <span class=\"post-link\">$2</span>
         </a>
-        <span class=\"commit-hash\">
-            <a href=\"https://github.com/nerdypepper/site/blob/master/posts/$1.md\" style=\"text-decoration: none\">
+        <div class="stats">
+            <span class=\"stats-number\">
                 $4
-            </a>
-        </span>
+            </span>
+            <span class="stats-unit">min</span>
+        </div>
     </div>
     "
 }
@@ -50,6 +61,7 @@ echo "
 echo "
 <body>
 <h1 class=\"heading\">n</h1>
+<h4 class=\"date\">nerdypepper's μblog</h4>
 " >> docs/index.html
 
 
@@ -70,10 +82,16 @@ for f in $posts; do
 
     # generate posts
     html=$(lowdown "$file")
-    commit="$(git log -n1 --oneline "$file" | sed -e 's/\s.*$//g')"
+    stats=$(wc "$file")
+    words="$(echo $stats | awk '{print $2}')"
+    lines="$(echo $stats | awk '{print $1}')"
+
+    r_time="$(read_time $words)"
+    height="$(height $lines)"
+
     post_title=$(title_wrapper "$id")
     post_date=$(date -r "$file" "+%d/%m %Y")
-    post_link=$(link_wrapper "${id%.*}" "$post_title" "$post_date" "$commit")
+    post_link=$(link_wrapper "${id%.*}" "$post_title" "$post_date" "$r_time" "$height")
 
     echo -ne "$post_link" >> docs/index.html
 
@@ -84,21 +102,22 @@ for f in $posts; do
         "./post.esh" \
         file="$file" \
         date="$post_date" \
-        commit="$commit" \
-        title="$post_title"
+        title="$post_title" \
+        read_time="$r_time" \
+        height="$height"
 
 done
 
 echo "
 <div class="separator"></div>
 <div class="footer">
-    <a href="https://github.com/nerdypepper"> Github </a>
+    <a href="https://github.com/nerdypepper">Github</a>
     · 
-    <a href="https://twitter.com/N3rdyP3pp3r"> Twitter </a>
+    <a href="https://twitter.com/N3rdyP3pp3r">Twitter</a>
     · 
-    <a href="mailto:nerdypepper@tuta.io"> Mail </a>
+    <a href="mailto:nerdypepper@tuta.io">Mail</a>
     · 
-    <a href="https://linkedin.com/in/nerdypepper"> LinkedIn </a>
+    <a href="https://linkedin.com/in/nerdypepper">LinkedIn</a>
 </div>
 </div>
 </body>
