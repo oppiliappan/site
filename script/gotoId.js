ab6d72b78ef4ecb77715217b04aed09c0342a684
Akshay <nerdypepper@tuta.io>
2020-01-27 11:16:06 +0530

    remote js

--- script/gotoId.js
+++
@@ -1,6 +0,0 @@
-function gotoId() {
-  if ( window.location.hash ) {
-    let hash = window.location.hash.substring(1);
-    showPost(hash);
-  }
-}
--- script/showPost.js
+++
@@ -1,8 +0,0 @@
-function showPost(id) {
-  let post = document.getElementById(id);
-  if (post.style.display == "none") {
-    post.style.display = "block";
-  } else {
-    post.style.display = "none";
-  }
-}
